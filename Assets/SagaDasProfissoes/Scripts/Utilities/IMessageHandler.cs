b627cfef28e57313313ffcaa50dfb3b342b025f7
Andre Luis da Cunha <andreluiscunha81@gmail.com>
2019-04-17 00:02:50 -0300

    Initial commit
    Added submodule Assets/DeadCow/Scripts/MyBox

--- Assets/SagaDasProfissoes/Scripts/Utilities/IMessageHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace AssemblyCSharp.SagaDasProfissoes.Scripts.Utilities
-{
-    public interface IMessageHandler
-    {
-        void OnError();
-        void OnSucess();
-    }
-}
